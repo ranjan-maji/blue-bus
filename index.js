faa4781e38ced69bfbb9f66497767b62c989fbbd
ranjan-maji <ranjanmaji906@gmail.com>
2023-04-11 14:44:43 +0530

    first-commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+
+const app = express()
+
+app.listen(5001, () => console.log('Api running port 5001'))
+
+app.get('/', (req, res) => res.json('My Server is running') )
